3d3c42e4c090b7a42d59775f38e6b77e2700e409
kubedav <davesade.42@gmail.com>
2017-03-21 15:11:54 +0100

    Removed email server.

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,17 +1,17 @@
-ActionMailer::Base.default_url_options = {
-  host: ENV.fetch('FRAB_HOST'),
-  protocol: ENV.fetch('FRAB_PROTOCOL')
-}
-if ENV['SMTP_ADDRESS']
-  %w(ADDRESS PORT DOMAIN USER_NAME PASSWORD AUTHENTICATION).each do |setting|
-    next unless ENV["SMTP_#{setting}"].present?
-    ActionMailer::Base.smtp_settings[setting.downcase.to_sym] = ENV["SMTP_#{setting}"]
-  end
-end
-if ENV['SMTP_NOTLS'].present?
-  ActionMailer::Base.smtp_settings.merge!(
-    enable_starttls_auto: false,
-    ssl: false,
-    tls: false
-  )
-end
+#ActionMailer::Base.default_url_options = {
+#  host: ENV.fetch('FRAB_HOST'),
+#  protocol: ENV.fetch('FRAB_PROTOCOL')
+#}
+#if ENV['SMTP_ADDRESS']
+#  %w(ADDRESS PORT DOMAIN USER_NAME PASSWORD AUTHENTICATION).each do |setting|
+#    next unless ENV["SMTP_#{setting}"].present?
+#    ActionMailer::Base.smtp_settings[setting.downcase.to_sym] = ENV["SMTP_#{setting}"]
+#  end
+#end
+#if ENV['SMTP_NOTLS'].present?
+#  ActionMailer::Base.smtp_settings.merge!(
+#    enable_starttls_auto: false,
+#    ssl: false,
+#    tls: false
+#  )
+#end
